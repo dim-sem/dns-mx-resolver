a142c400ca16206033408e713f77a81df48d3b31
dim-sem <dim_semenov@mail.ru>
2015-10-27 17:36:17 +0300

    added argsparse

--- dns_mx_resolver.py
+++ dns_mx_resolver.py
@@ -7,22 +7,34 @@ def dns_mx_resolver():
     pass
 
 def ArgsParser():
+    # Initialize argparse    
     cmd_parser = argparse.ArgumentParser(description='Process input [output] file(s).')
+    # Define command-line arguments
     cmd_parser.add_argument('input_file', nargs=1, help = 'input csv file')
     cmd_parser.add_argument('output_file', nargs='?', help = 'output file')
     args = cmd_parser.parse_args()
-    print args.input_file
-    print args.output_file
+    #    print args.input_file
+    #    print args.output_file
+    # Open input and output files
     try:
         in_file = open(args.input_file[0],'r')
     except IOError, e:
         print e
-    try:
-        out_file = open(args.output_file[0],'w+') 
-    except IOError, e:
-        print e
+    if args.output_file is not None:
+        try:
+            out_file = open(args.output_file[0],'w+') 
+        except IOError, e:
+            print e
+    else:
+        args.output_file = []
+        args.output_file.append(args.input_file[0]+'_processed.csv')
+        try:
+            out_file = open(args.output_file[0],'w+')  
+        except IOError, e:
+            print e        
     print args
     return args
+
 
 if __name__ == "__main__":
     print os.getcwd()
