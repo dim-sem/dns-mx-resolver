cafaf2ef924fddd8e4f04f44673ccee4e8c19b23
dim-sem <dim-sem@users.noreply.github.com>
2015-11-02 20:09:44 +0300

    Delete test.py
    Obsolete test module.

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import dns.resolver
-
-email_address = 'gmail.com'
-mx = []
-for mx_record in dns.resolver.query(email_address,'MX'):
-    mx.append(mx_record.to_text().split(' ')[1][:-1])
-print sorted(mx)
